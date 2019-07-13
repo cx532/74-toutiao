0e13ef55971f462b540dc1444f2615684e877a52
cx532 <1071666350@qq.com>
2019-07-13 11:50:14 +0800

    请求响应拦截器

--- src/main.js
+++ src/main.js
@@ -3,7 +3,14 @@ import App from './App.vue'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from '@/router'
-import axios from 'axios'
+import axios from '@/api/axios'
+// import axios from 'axios'
+// // 基准路径
+// axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
+// // token认证
+// axios.defaults.headers = {
+//   Authorization: 'Bearer ' + JSON.parse(window.sessionStorage.getItem('toutiao')).token
+// }
 Vue.prototype.axios = axios
 Vue.use(Element)
 Vue.config.productionTip = false
